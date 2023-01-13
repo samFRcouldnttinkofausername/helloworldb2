32e78bebb266c89c188be558323a8d626f67d41c
samFRcouldnttinkofausername <122544219+samFRcouldnttinkofausername@users.noreply.github.com>
2023-01-13 09:56:03 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.showString("yippie!")
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+    music.playMelody("D F E F G G F E ", 120)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("bye")
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 500, 499, 255, 0, 750, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+    music.playMelody("A G F E D C C C ", 120)
+})
+basic.showString("WAHOO")
 basic.showIcon(IconNames.Happy)
-music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+music.playMelody("A F B G C5 B A G ", 120)
